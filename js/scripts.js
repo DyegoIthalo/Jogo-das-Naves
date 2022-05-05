7f634b29902119d11401cf742cf5f70c888ec484
DyegoIthalo <dyegoithalo11@hotmail.com>
2022-05-05 02:24:26 -0300

    exibir pontuação, quantidade de amigos salvos, perdidos e criação da função placar (atualização de variáveis, adicionando estilos)

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,14 @@ function start() {
   $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
   $("#fundoGame").append("<div id='inimigo2' ></div>");
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+  $("#fundoGame").append("<div id='placar'></div>");
 
   //Principais variáveis do jogo
   var podeAtirar = true;
   var fimdejogo = false;
+  var pontos = 0;
+  var salvos = 0;
+  var perdidos = 0;
   var jogo = {};
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
@@ -41,6 +45,7 @@ function start() {
     moveinimigo2();
     moveamigo();
     colisao();
+    placar();
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -164,6 +169,7 @@ function start() {
 
     // Disparo com o inimigo1
     if (colisao3.length > 0) {
+      pontos = pontos + 100;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -177,6 +183,7 @@ function start() {
 
     // Disparo com o inimigo2
     if (colisao4.length > 0) {
+      pontos = pontos + 50;
       inimigo2X = parseInt($("#inimigo2").css("left"));
       inimigo2Y = parseInt($("#inimigo2").css("top"));
       $("#inimigo2").remove();
@@ -189,12 +196,14 @@ function start() {
 
     // jogador com o amigo
     if (colisao5.length > 0) {
+      salvos++;
       reposicionaAmigo();
       $("#amigo").remove();
     }
 
     //Inimigo2 com o amigo
     if (colisao6.length > 0) {
+      perdidos++;
       amigoX = parseInt($("#amigo").css("left"));
       amigoY = parseInt($("#amigo").css("top"));
       explosao3(amigoX, amigoY);
@@ -281,4 +290,17 @@ function start() {
       tempoExplosao3 = null;
     }
   } // Fim da função explosao3
+
+  //função placar
+  function placar() {
+    $("#placar").html(
+      "<h2> Pontos: " +
+        pontos +
+        " Salvos: " +
+        salvos +
+        " Perdidos: " +
+        perdidos +
+        "</h2>"
+    );
+  } //fim da função placar()
 } // Fim da função start
